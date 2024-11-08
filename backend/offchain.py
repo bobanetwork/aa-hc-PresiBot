166c1f9602aca0baad1acb667f9e1e37de6d7730
wsdt <kevin.riedl.privat@gmail.com>
2024-11-08 08:58:08 +0100

    fix: render conf

--- backend/offchain.py
+++ backend/offchain.py
@@ -3,11 +3,14 @@ from openai_offchain import openai_create_question
 from openai_offchain import select_best_answer
 from hybrid_compute_sdk import HybridComputeSDK
 import logging
+import os
 
 # Initialize logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 def server_loop():
+    port = int(os.environ.get("PORT", os.environ.get("OC_LISTEN_PORT", 1234)))
+
     # new sdk instance
     sdk = HybridComputeSDK()
     # prepare the server
